bdaa3f8aade165ba5567ab99bfe34028d5e09be3
Luan Persini <luanpersini@hotmail.com>
2022-09-01 20:07:58 -0300

    chore: install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { HttpExceptionFilter } from 'src/presentation/filters/httpException.filter'
 import { AppModule } from './app.module'
 
@@ -15,7 +15,11 @@ async function bootstrap() {
     })
   )
   app.useGlobalFilters(new HttpExceptionFilter())
-  
+
+  const options = new DocumentBuilder().setTitle('equipments-api').setDescription('Printer Equipments').setVersion('1.0.0').build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('docs', app, document)
+
   await app.listen(port)
 }
 
